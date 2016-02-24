f461177b937c462afbb3fd990aba394d095b456c
1423177715@qq.com <1423177715@qq.com>
2016-02-24 16:41:24 +0800

    create project

---
+++ EasyUICodeFirstEF/ViewWeb/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using ViewWeb.Filters;
+using ViewWeb.Models;
+
+namespace ViewWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Main/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EasyUICodeFirstEF/ViewWeb/Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewWeb.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+        // [HttpPost]
+        public ActionResult Index()
+        {
+            ViewBag.UserName = "test";
+            ViewBag.RoleName = "manager";
+            return View();
+        }
+
+        //
+        // GET: /Main/
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+    }
+}
